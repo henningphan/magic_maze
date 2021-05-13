cc5375c73aa64bee9e7a30f61aaf2662cd15e784
Henning Phan <henning.phan@zenseact.com>
2021-05-13 14:51:12 +0200

    refactor pass all tests

--- magicmaze/solution.py
+++ magicmaze/solution.py
@@ -5,11 +5,11 @@ from pprint import pprint
 class State:
     def __init__(self):
         self.avatar = None
+        self.tick = 0
         self.powerup = 1
         self.crate = 5
         self.player = 20
         self.immolation = -10
-        self.tick = 0
         self.maze = {}
         self.powerups = []
         self.vortexes = []
@@ -24,17 +24,17 @@ class State:
                      range(maze_width), range(maze_height))}
 
         for xy in (self.to_pos(xy) for xy in walls):
-            del self.maze[xy]
+            del maze[xy]
         self.maze = maze
         return maze
 
     def update_players(self, str_players):
-        player_to_xy = {player: to_pos(str_xy)
+        player_to_xy = {player: self.to_pos(str_xy)
                 for player, str_xy in str_players.items()}
         self.players.append(player_to_xy)
 
-    def update_vortexes(self, vortexes):
-        self.vortexes.append([self.to_pos(v) for v in vortexes])
+    def update_vortexes(self, str_vortexes):
+        self.vortexes.append([self.to_pos(v) for v in str_vortexes])
 
     def update_powerups(self, str_powerups):
         powerups = [self.to_pos(p) for p in str_powerups]
@@ -42,15 +42,16 @@ class State:
             if xy in powerups:
                 self.power[-1][p] += 1
 
+    def update_crates(self, str_crates):
+        self.crates.append([self.to_pos(c) for c in str_crates])
+
     def to_pos(self, str_pos):
         x, y = str_pos.replace("(","").replace(")","").split(",")
         return int(x), int(y)
 
-    def init_players(self, your_avatar, players):
-        self.avatar = your_avatar
-        self.players = [players]
+    def init_players(self, players):
+        self.players = [{p: self.to_pos(xy) for p,xy in players.items()}]
         self.power = [{p: 1 for p in players.keys()}]
-        self.power[-1][self.avatar] = 1
 
     @property
     def my_pos(self):
@@ -100,12 +101,8 @@ def neighbours(state, pos):
 class Solution:
     def __init__(self, api):
         self.api = api
-        self.powerup = 1
-        self.crate = 5
-        self.player = 20
-        self.immolation = -10
+        self.state = State()
         self.tick = 0
-
 
     def initialize_maze_data(self, maze_width, maze_height, walls,
 your_avatar):
@@ -120,12 +117,8 @@ static maze
         :type your_avatar: string
         """
         print("whoami: ", your_avatar)
-        self.avatar = your_avatar
-        self.maze = {xy: None for xy in product(range(maze_width),
-range(maze_height))}
-
-        for xy in (self.position(xy) for xy in walls):
-            del self.maze[xy]
+        self.state.avatar = your_avatar
+        self.state.init_map(maze_width, maze_height, walls)
 
         assert maze_width == maze_height
 
@@ -147,18 +140,17 @@ do both
         :type vortexes: List[string]
         :type players: Dict[string, string]
         """
+        print("avatar:", self.state.avatar())
+        if self.tick == 0:
+            self.state.init_players(players)
         self.tick += 1
-        my_pos = self.position(players[self.avatar])
-        print("whoami: ", self.avatar)
-        pprint(my_pos)
-        crates = [self.position(c) for c in crates]
-        powerups = [self.position(p) for p in powerups if self.position(p) != my_pos]
+        self.state.update_players(players)
+        self.state.update_powerups(powerups)
+        self.state.update_vortexes(vortexes)
 
-        vortexes = [self.position(v) for v in vortexes]
-        my_pos = self.position(players[self.avatar])
 
-        distance = Solution.calculate_distance(self.maze, my_pos, crates)
-        action, score = Solution.next_action(my_pos, distance, crates, powerups, vortexes, players, self.maze)
+        distance = calculate_distance(self.state, self.my_pos)
+        action, score = next_action(my_pos, distance, crates, powerups, vortexes, players, self.maze)
         if action == (0,0):
             copypaste(my_pos, distance, crates, powerups, vortexes, players, self.maze)
         print("best action: ", action, score)
@@ -167,36 +159,6 @@ do both
         else:
             pos = action
             self.api.move(*Solution.move_to(distance, pos))
-
-
-    @staticmethod
-    def calculate_distance(maze, my_pos, blocked_pos=None):
-        """
-        :type blocked_pos: Tuple(int, int), positions one cannot move to
-        """
-        blocked_pos = blocked_pos if blocked_pos else []
-        distance = maze.copy()
-        queue = [(my_pos, [])]
-        while queue:
-            pos, way = queue.pop(0)
-            if distance[pos] is None:
-                distance[pos] = [pos] + way
-                new_way = [pos] + way
-                neighbours = ((pos, new_way.copy()) for pos in
-                        Solution.neighbours(pos, maze) if distance[pos] is None and pos not in blocked_pos)
-                queue.extend(neighbours)
-        return distance
-
-    @staticmethod
-    def neighbours(pos, maze):
-        """
-        :type pos: tuple(int, int)
-        :type maze: dict[tuple(int int)] = None
-        """
-        all_neighbours = [(pos[0]-1, pos[1]), (pos[0]+1, pos[1]),
-                (pos[0], pos[1]-1), (pos[0], pos[1]+1)]
-        valid_neighbours = [n for n in all_neighbours if n in maze]
-        return valid_neighbours
 
     @staticmethod
     def move_to(distance, pos):
@@ -212,42 +174,29 @@ do both
         y_change = y2-y1
         return (x_change, y_change)
 
-    @staticmethod
-    def next_action(my_pos, distance, crates, powerups, vortexes, players, maze):
-        bomb_score = Solution.eval_bomb(my_pos, crates, vortexes)
-        pos_score = [(pos, Solution.position_score(pos, distance, crates, powerups, vortexes, maze)) for pos in maze.copy().keys()]
-        scores = pos_score + [bomb_score]
-        scores.sort(key=lambda tup: tup[1], reverse=True)
-        return scores[0]
+def next_action(state):
+    bomb_score = eval_bomb(state)
+    distance = calculate_distance(state, state.my_pos)
+    pos_score = [(pos, position_score(pos, distance, state.crates, state.powerups, state.vortexes, state.maze)) for pos in state.maze.copy().keys()]
+    scores = pos_score + [bomb_score]
+    scores.sort(key=lambda tup: tup[1], reverse=True)
+    return scores[0]
 
+def eval_bomb(state):
+    if state.my_pos in state.vortexes[-1]:
+        return ("bomb", 0)
+    else:
+        return ("bomb", crates_around_pos(state.my_pos, state.crates[-1])*state.crate)
 
-    @staticmethod
-    def eval_bomb(my_pos, crates, vortexes):
-        if my_pos in vortexes:
-            return ("bomb", 0)
-        else:
-            return ("bomb", Solution.crates_around_pos(my_pos, crates)*10)
-    @staticmethod
-    def crates_around_pos(pos, crates):
-        next_to_me = [(pos[0]-1, pos[1]), (pos[0]+1, pos[1]),
-                (pos[0], pos[1]-1), (pos[0], pos[1]+1)]
-        return len([c for c in crates if c in next_to_me])
+def crates_around_pos(pos, crates):
+    next_to_me = [(pos[0]-1, pos[1]), (pos[0]+1, pos[1]),
+            (pos[0], pos[1]-1), (pos[0], pos[1]+1)]
+    return len([c for c in crates if c in next_to_me])
 
-    @staticmethod
-    def position_score(pos, distance, crates, powerups, vortexes, maze):
-        try:
-            scores = (Solution.eval_bomb(pos, crates, vortexes)[1]*0.8 +
-                    (1 if pos in powerups else 0))/len(distance[pos])
-            return scores
-        except TypeError as e:
-            return 0
-
-
-def copypaste(my_pos, distance, crates, powerups, vortexes, players, maze):
-    print("my_pos=", my_pos)
-    print("distance=", distance)
-    print("crates=", crates)
-    print("powerups=", powerups)
-    print("vortexes=", vortexes)
-    print("players=", players)
-    print("maze=", maze)
+def position_score(pos, distance, crates, powerups, vortexes, maze):
+    try:
+        scores = (eval_bomb(pos, crates, vortexes)[1]*0.8 +
+                (1 if pos in powerups else 0))/len(distance[pos])
+        return scores
+    except TypeError as e:
+        return 0
--- test/test_generic.py
+++ test/test_generic.py
@@ -7,48 +7,71 @@ from itertools import product
 def test_neighbours():
     max_x = 6
     max_y = 6
-    maze = {xy: None for xy in product(range(max_x), range(max_y))}
+    state = State()
+    state.init_map(max_x, max_y, [])
     for xy in product([0, max_x-1], [0, max_y-1]):
-        neigh = Solution.neighbours(xy, maze)
+        neigh = mm.neighbours(state, xy)
         assert len(neigh) == 2
 
 def test_calculate_distance_no_blocks():
     """test calculate distance when the whole map is clear from obstacles"""
     max_x = 6
     max_y = 6
-    maze = {xy: None for xy in product(range(max_x), range(max_y))}
+    state = State()
+    state.avatar = "elf"
+    state.init_players({"elf": "(0,0)"})
+    state.init_map(max_x, max_y, [])
     for xy in product(range(max_x), range(max_y)):
         for xy2 in product(range(max_x), range(max_y)):
-            distance = Solution.calculate_distance(maze, xy, [])
+            state.update_players({"elf": str(xy)})
+            distance = mm.calculate_distance(state, xy)
             assert len(distance[xy2]) == abs(xy2[0]-xy[0]) + abs(xy2[1]-xy[1]) + 1
 
 def test_calculate_distance_w_blocks():
     """test calculate distance when the whole map with obstacles"""
     max_x = 6
     max_y = 1
-    maze = {xy: None for xy in product(range(max_x), range(max_y))}
-    distance = Solution.calculate_distance(maze, (0, 0), [(1,0)])
+    state = State()
+    state.avatar = "elf"
+    state.init_players({"elf": "(0,0)"})
+    state.init_map(max_x, max_y, ["(1,0)"])
+    distance = mm.calculate_distance(state, (0, 0))
     print(distance[(2,0)])
     assert distance[(2,0)] is None
 
 def test_move_to():
     max_x = 6
     max_y = 6
-    maze = {xy: None for xy in product(range(max_x), range(max_y))}
-    distance = Solution.calculate_distance(maze, (0, 0), [])
+
+    state = State()
+    state.init_map(max_x, max_y, [])
+    state.avatar = "elf"
+    state.init_players({"elf": "(0,0"})
+    distance = mm.calculate_distance(state, (0, 0))
     x, y = Solution.move_to(distance, (0,0))
+    assert x == 0
     assert y == 0
+    x, y = Solution.move_to(distance, (0,1))
+    assert x == 0
+    assert y == 1
+    x, y = Solution.move_to(distance, (1,0))
+    assert x == 1
+    assert y == 0
+    x, y = Solution.move_to(distance, (1,1))
+    assert x+y == 1
 
 def test_state_calculate_distance():
     state = State()
     state.init_map(6,6,[])
-    state.init_players("elf", {"elf": (0,0)})
+    state.avatar="elf"
+    state.init_players({"elf": "(0,0)"})
     distance = mm.calculate_distance(state, (0,0))
     assert len(distance) == 36
     
 def test_update_powerups():
     state = State()
-    state.init_players("elf", {"elf": (0,0)})
+    state.avatar="elf"
+    state.init_players({"elf": "(0,0)"})
     assert state.my_power == 1
     state.update_powerups(["(0,0)"])
     assert state.my_power == 2
