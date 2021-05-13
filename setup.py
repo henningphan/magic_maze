0b4a3082944490ad830ebcc43b2a2ee89a18e303
Henning Phan <henning.phan@zenseact.com>
2021-05-13 13:42:52 +0200

    update package name to magicmaze

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 print(find_packages())
 setup(
-    name='magic_maze',
+    name='magicmaze',
     version='0.1.0',
     packages=find_packages(include=".")
 )
